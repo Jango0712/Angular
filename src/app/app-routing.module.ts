441b4842bc93ad3528061cb3e12edf5c31885b0c
Jango0712 <gomezmarchanj@gmail.com>
2023-08-27 19:17:32 +0100

    pagina profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
-  {path: "", component:HomeComponent},
-  {path: "register", component:RegisterComponent}
+  {path:'',redirectTo:'home', pathMatch:'full'},
+  {path:"home", component:HomeComponent},
+  {path: "register", component:RegisterComponent},
+  {path: "profile", component: ProfileComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  FormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
