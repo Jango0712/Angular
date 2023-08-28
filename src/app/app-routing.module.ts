81f2e503b4b725abe34a472a00fb60bc8ad9d93b
Jango0712 <gomezmarchanj@gmail.com>
2023-08-28 20:46:42 +0100

    dia3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { FormsModule } from '@angular/forms';
+import { BooksComponent } from './pages/books/books.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home', pathMatch:'full'},
   {path:"home", component:HomeComponent},
   {path: "register", component:RegisterComponent},
-  {path: "profile", component: ProfileComponent}
+  {path: "profile", component: ProfileComponent},
+  {path: 'books', component: BooksComponent}
 ];
 
 @NgModule({
---
+++ src/app/pages/books/books.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Book } from 'src/app/models/book';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent {
+public books: Book[];
+constructor(){
+this.books = [
+  new Book('El Principito', 'Aventuras', 'José Carlos', 99, 'https://m.media-amazon.com/images/I/81E-RNGwAIL._AC_UF1000,1000_QL80_.jpg')
+]
+}
+add(title:string, type:string, author:string, price:number, photo:string, id_book:number, id_user:number){
+this.books.push(new Book(title, type, author,price, photo, id_book, id_user))
+}
+}
