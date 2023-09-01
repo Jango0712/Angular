feb56eba08bda5c77bbf555a03f9bcf6fd811ca0
Jango0712 <gomezmarchanj@gmail.com>
2023-09-01 14:20:04 +0100

    dia

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Book } from 'src/app/models/book';
+
+
 
 @Component({
   selector: 'app-books',
@@ -10,10 +12,15 @@ export class BooksComponent {
 public books: Book[];
 constructor(){
 this.books = [
-  new Book('El Principito', 'Aventuras', 'José Carlos', 99, 'https://m.media-amazon.com/images/I/81E-RNGwAIL._AC_UF1000,1000_QL80_.jpg')
+  new Book('El Principito', 'Aventuras', 'José Carlos', 99, 'https://m.media-amazon.com/images/I/81E-RNGwAIL._AC_UF1000,1000_QL80_.jpg'),
+ 
 ]
 }
 add(title:string, type:string, author:string, price:number, photo:string, id_book:number, id_user:number){
 this.books.push(new Book(title, type, author,price, photo, id_book, id_user))
+}
+
+delete(id_book:number){
+  this.books = this.books.filter(book => book.id_book != id_book)
 }
 }
